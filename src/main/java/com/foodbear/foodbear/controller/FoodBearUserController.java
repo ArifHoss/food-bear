35c2e8106f9bd11455b584ea331d03943201399d
arif <hossaininfo@ymail.com>
2022-05-09 17:47:52 +0200

    optimized import from FoodBearUserController

--- src/main/java/com/foodbear/foodbear/controller/FoodBearUserController.java
+++ src/main/java/com/foodbear/foodbear/controller/FoodBearUserController.java
@@ -1,15 +1,11 @@
 package com.foodbear.foodbear.controller;
 
 import com.foodbear.foodbear.entities.FoodBearUser;
-import com.foodbear.foodbear.entities.UserAddress;
 import com.foodbear.foodbear.service.FoodBearUserService;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 @Data
 @RestController
@@ -19,7 +15,6 @@ import java.util.List;
 public class FoodBearUserController {
 
     private final FoodBearUserService foodBearUserService;
-    //private UserAddress userAdressService;
 
     @GetMapping
     public List<FoodBearUser> getAllUsers(){
